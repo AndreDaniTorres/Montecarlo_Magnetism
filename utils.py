50f1c4067d4b9fc1d63544733597780ecb99438e
AndreDaniTorres <60939382+AndreDaniTorres@users.noreply.github.com>
2023-06-04 01:34:09 -0500

    upload

---
+++ utils.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Barra de progreso para limpieza en analisis.
+# https://stackoverflow.com/questions/3173320/text-progress-bar-in-terminal-with-block-characters 
+def progressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
+
+def save_lists_as_columns(filename, list1, list2):
+    """Función para guardar archivos con resultados"""
+    np.savetxt(filename, np.column_stack((list1, list2)), fmt='%f')
+
+
+
+def plot_snapshots(matrix,T):
+     # Colormap for red-blue gradient
+    cmap = plt.cm.RdBu 
+    plt.imshow(matrix, cmap=cmap, interpolation='nearest', vmin=-1, vmax=1)
+
+    # Save the plot as a PNG image
+    plt.savefig(f'plots/ferro_ising_T_{T}.png')
